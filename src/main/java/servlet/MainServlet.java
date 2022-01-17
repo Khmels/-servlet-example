f7f6f28bb83b4c4cac60ca3749d4be347631fad6
khmels <ser.khmel@gmail.com>
2022-01-17 23:34:14 +0200

    v2. Post, form and static. 50%

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -2,11 +2,13 @@ package servlet;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = {"/another-way", "/super-servlet/*"})
 public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
